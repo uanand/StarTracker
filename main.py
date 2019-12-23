dde28a835410225aded4ccfc428bd71f958a829d
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-12-23 17:37:54 +0800

    Add the str catalogie and sample distance table.

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath('./lib'))
+import starTracker
+import utils
+
+catalogueFileName = './dataset/scHIP4to6p5.txt'
+FOVDegree = 20
+
+catalogue = starTracker.readCatalogue(catalogueFileName)
+catalogue = starTracker.addCartesianCoordinates(catalogue)
+dTable = starTracker.createDistanceTable(catalogue,FOVDegree)
+# print (catalogue.shape)
+
+
